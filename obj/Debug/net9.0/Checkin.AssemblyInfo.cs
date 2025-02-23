be595870f6f1a79b0ad5fb11c09c865e4339eaf1
Chris Smith <csmith@southcentralfoundation.com>
2025-02-23 01:44:01 -0800

    added a readme.md file with generalized charter of this project

--- obj/Debug/net9.0/Checkin.AssemblyInfo.cs
+++ obj/Debug/net9.0/Checkin.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Checkin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cab91343507f24a6cca26363798937d6aabd4868")]
 [assembly: System.Reflection.AssemblyProductAttribute("Checkin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Checkin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
